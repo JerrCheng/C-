4e1733793e94fcc49a760ddb8a168775a31776a2
Jerry cheng <10488043+jerry-cheng123@user.noreply.gitee.com>
2022-06-22 12:27:28 +0000

    C语言——简易版通讯录

---
+++ 简易版通讯录test.c
@@ -0,0 +1,67 @@
+#include"Contact.h"
+
+void menu()
+{
+	printf("*************************************\n");
+	printf("******* 1.add      2.del    *********\n");
+	printf("******* 3.search   4.modify *********\n");
+	printf("******* 5.sort     6.print  **********\n");
+	printf("******* 7.clear    0.exit    *******  \n");
+	printf("*************************************\n");
+	printf("*************************************\n");
+
+}
+
+
+void test()
+{
+	//创建通讯录
+	Contact con;       //通讯录
+	InitContact(&con); // 初始化通讯录
+	int input = 0;
+
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			AddContact(&con);
+			break;
+		case DEL:
+			DelContact(&con);
+			break;
+		case SEARCH:
+			SearchContact(&con);
+			break;
+		case MODIFY:
+			ModifyContact(&con);
+			break;
+		case SORT:
+			SortContact(&con);
+			break;
+		case PRINT:
+			PrintContact(&con);
+			break;
+		case CLEAR:
+			ClearContact(&con);
+			break;
+		case EXIT:
+			printf("退出通讯录\n");
+			break;
+		default:
+			printf("选择错误\n");
+			break;
+		}
+	} while (input);
+}
+
+
+
+int main()
+{
+	test();
+	return 0;
+}
